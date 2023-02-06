94ba79736ef1d4c9fae0be917008a29f98b75d97
Roo2409 <roobikapoor@hotmail.com>
2023-02-06 14:21:12 +0000

    finalchange

--- manual_rps.py
+++ manual_rps.py
@@ -1,17 +1,17 @@
 import random
 def get_computer_choice():
-   list1 = ["rock","paper","scissor"]
+   list1 = ["Rock","Paper","Scissors"]
    return random.choice(list1)
       
 def get_user_choice(): 
   pick_one = input("choose one from list")  
-  # if pick_one in list1:
+ # if pick_one in list1:
   return pick_one
   # else:
   #    print("wrong letter")
 
 
-# list1 = ["rock","paper","scissor"]
+#list1 = ["rock","paper","scissor"]
 t1= get_computer_choice()
 print(t1)
 t2 = get_user_choice()
