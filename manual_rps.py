bca9d6b9efd720ea4158e87cdee54a6a3632735f
Roo2409 <roobikapoor@hotmail.com>
2023-02-06 13:37:37 +0000

    define function

--- manual_rps.py
+++ manual_rps.py
@@ -1,8 +1,16 @@
 import random
+# ask_the_user: input("choose one from list")  
 def get_computer_choice():
+    return random.choice(list)
     
-    
-    
-    
-    def get_user_choice():
-        
+# ask_the_user: input("choose one from list")       
+def get_user_choice(): 
+  ask_the_user: input("choose one from list")  
+  if ask_the_user == list:
+     print(ask_the_user)
+
+
+list = ["rock","paper","scissor"]
+# ask_the_user: input("choose one from list")  
+t1 = get_user_choice()
+print(t1)
