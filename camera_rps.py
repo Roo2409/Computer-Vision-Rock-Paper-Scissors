325d1ac40e3bcf2e5b84e8e13ef28e60185f345a
Roo2409 <roobikapoor@hotmail.com>
2023-02-08 17:08:42 +0000

    redoing 1 and 2

--- camera_rps.py
+++ camera_rps.py
@@ -1,36 +1,34 @@
 import cv2
+from keras.models import load_model
 import numpy as np
-import random
-
+import time
 def get_prediction():
-    return 
-cv2.imshow()
-def timetime():
-    print("you can choose rock")
-timetime()
-computer_wins = 0
-user_wins=0
-round_played = 0
-list1 = ["Rock","Paper","Scissors"]
-while computer_wins <3 and user_wins <3:
-  print ("Starting round ", round_played + 1)
-  print ("-----------------------")
-  computer_choice = random.choice(list1)
-  user_choice = input("choice rock , paper, scissor")
-  
-  print (f"computer_choice choose : {computer_choice} and user_choice choose {user_choice}")
-  if computer_choice == user_choice:
-    print("It's a tie!")
-  elif (computer_choice == "Rock"and user_choice == "Scissors") or (computer_choice == "Paper" and user_choice == "Rock") or (computer_choice == "Scissors"and user_choice == "Paper"):
-    print("computer_choice You win!")
-    computer_wins += 1
-  else: 
-    print("user_choice you win")
-    user_wins +=1
-round_played +=1
-if computer_wins > user_wins:
-  print ("computer_wins is the winner")
-elif user_wins > computer_wins:
-  print ("user_wins is the winner")
-else:
-   print("the game is over")
+  model = load_model('keras_model.h5')
+  cap = cv2.VideoCapture(0)
+  data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+  start_time = time.time()
+  while time.time() < start_time+4: 
+    ret, frame = cap.read()
+    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+    image_np = np.array(resized_frame)
+    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+    data[0] = normalized_image
+    prediction = model.predict(data)
+    cv2.imshow('frame', frame)
+    # Press q to close the window 
+    print(prediction)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+      break
+  cap.release()
+  # Destroy all the windows
+  cv2.destroyAllWindows()
+  return prediction
+
+
+list1 = ["rock","paper","scissors","nothing"]
+
+t1 = get_prediction()
+print(t1)
+# cv2.imshow()
+
+
